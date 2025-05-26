9a051a0ed181b79ba6f2e92e59fb8fda75444903
unknown <nicolasheyer@gmail.com>
2025-05-26 22:13:30 +0200

    Create Product class

---
+++ products.py
@@ -0,0 +1,84 @@
+class Product:
+    def __init__(self, name: str, price: float, quantity: int):
+        if not name:
+            raise ValueError("Product name can't be empty.")
+        if price < 0:
+            raise ValueError("Price can't be negative.")
+        if quantity < 0:
+            raise ValueError("Quantity can't be negative.")
+
+        # Initialize instance variables
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.active = True
+
+
+    def get_quantity(self) -> int:
+        """Return the current quantity of the product."""
+        return self.quantity
+
+
+    def set_quantity(self, quantity: int):
+        """Set quantity. Deactivate product if quantity reaches zero."""
+        if quantity < 0:
+            raise ValueError("Quantity cannot be negative.")
+        self.quantity = quantity
+        if self.quantity == 0:
+            self.deactivate()
+
+
+    def is_active(self) -> bool:
+        """Check if the product is active."""
+        return self.active
+
+
+    def activate(self):
+        """Activate the product."""
+        self.active = True
+
+
+    def deactivate(self):
+        """Deactivate the product."""
+        self.active = False
+
+
+    def show(self) -> str:
+        """Return a string representation of the product."""
+        return f"{self.name}, Price: {self.price}, Quantity: {self.quantity}"
+
+
+    def buy(self, quantity: int) -> float:
+        """Purchase process: Returns the total price, updates quantity,
+        and deactivates if warehouse is empty."""
+        if not self.active:
+            raise Exception(f"Product '{self.name}' is not active.")
+        if quantity <= 0:
+            raise ValueError("Quantity must be positive.")
+        if quantity > self.quantity:
+            raise Exception(
+                f"The requested {quantity} of '{self.name}' is not on stock (available {self.quantity})."
+            )
+
+        total_price = self.price * quantity
+        # Update stock
+        self.set_quantity(self.quantity - quantity)
+        return total_price
+
+def main():
+    bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
+    mac = Product("MacBook Air M2", price=1450, quantity=100)
+
+    print(bose.buy(50))
+    print(mac.buy(100))
+    print(mac.is_active())
+
+    bose.show()
+    mac.show()
+
+    bose.set_quantity(1000)
+    bose.show()
+
+if __name__ == "__main__":
+    main()
+
